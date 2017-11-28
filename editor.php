9b223669df8b17669013d0e94c4278bc3a1666e6
Valentin Popov <info@valentineus.link>
2017-11-29 01:40:44 +0400

    Template editor

---
+++ editor.php
@@ -0,0 +1,81 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The token editor.
+ *
+ * @package   tool_managertokens
+ * @copyright 2017 "Valentin Popov" <info@valentineus.link>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . "/../../../config.php");
+require_once(__DIR__ . "/classes/editor_form.php");
+require_once(__DIR__ . "/lib.php");
+
+require_once($CFG->libdir . "/adminlib.php");
+
+/* Optional parameters */
+$tokenid = optional_param("tokenid", 0, PARAM_INT);
+
+/* Link generation */
+$urlparameters = array("tokenid" => $tokenid);
+$baseurl       = new moodle_url("/admin/tool/managertokens/editor.php", $urlparameters);
+$managertokens = new moodle_url("/admin/tool/managertokens/index.php");
+
+/* Configure the context of the page */
+admin_externalpage_setup("tool_managertokens", "", null, $baseurl, array());
+$context = context_system::instance();
+
+/* Create an editing form */
+$mform = new token_editor_form($PAGE->url);
+
+/* Cancel processing */
+if ($mform->is_cancelled()) {
+    redirect($managertokens);
+}
+
+/* Getting the data */
+$tokenrecord = new stdClass();
+if ($editing = boolval($tokenid)) {
+    $tokenrecord = tool_managertokens_find_record($tokenid);
+    $mform->set_data($tokenrecord);
+}
+
+/* Processing of received data */
+if ($data = $mform->get_data()) {
+    if ($editing) {
+        $data->id = $tokenid;
+        tool_managertokens_update_record($data);
+        redirect($managertokens, new lang_string("update", "moodle"));
+    } else {
+        tool_managertokens_create_record($data);
+        redirect($managertokens, new lang_string("create", "moodle"));
+    }
+}
+
+/* The page title */
+$titlepage = new lang_string("editsettings", "moodle");
+$PAGE->navbar->add($titlepage);
+$PAGE->set_heading($titlepage);
+$PAGE->set_title($titlepage);
+echo $OUTPUT->header();
+
+/* Displays the form */
+$mform->display();
+
+/* Footer */
+echo $OUTPUT->footer();
