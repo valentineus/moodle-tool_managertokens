a7e10590ebadad6c40e9543bb9f86d543ec85e27
Valentin Popov <info@valentineus.link>
2017-12-03 21:00:02 +0400

    Change version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined("MOODLE_INTERNAL") || die();
 
 $plugin->component = "tool_managertokens";
 $plugin->maturity  = "MATURITY_RC";
-$plugin->release   = "0.1.0 (Build: 2017120200)";
+$plugin->release   = "0.2.0 (Build: 2017120300)";
 $plugin->requires  = 2016112900;
-$plugin->version   = 2017120200;
+$plugin->version   = 2017120300;
